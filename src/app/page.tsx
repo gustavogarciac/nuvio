daacaa733fe1e2c503b1f789ed0cfbfce6854a36
gustavo <gustavogarciaci1@gmail.com>
2024-09-01 15:31:42 -0300

    feat: jotai workspace state management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+'use client'
+
+import { useEffect, useMemo } from 'react'
+
+import { UserButton } from '@/features/auth/components/user-button'
+import { useCreateWorkspaceModal } from '@/features/store/use-create-workspace-modal'
+import { useGetWorkSpaces } from '@/features/workspaces/api/use-get-workspaces'
+
 export default function Home() {
-  return <main>Home</main>
+  const [open, setOpen] = useCreateWorkspaceModal()
+  const { data, isLoading } = useGetWorkSpaces()
+
+  const workspaceId = useMemo(() => data?.[0]?._id, [data])
+
+  useEffect(() => {
+    if (isLoading) return
+
+    if (workspaceId) {
+      console.log('Redirect to workspace')
+    } else if (!open) {
+      setOpen(true)
+    }
+  }, [workspaceId, isLoading, open, setOpen])
+
+  return (
+    <main>
+      <UserButton />
+    </main>
+  )
 }
