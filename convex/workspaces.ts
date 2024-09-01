1279f0d223b4a4dda29c13f35617a42108010720
gustavo <gustavogarciaci1@gmail.com>
2024-09-01 18:13:31 -0300

    feat: member table to relate workspace and user

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -3,6 +3,15 @@ import { v } from 'convex/values'
 import { mutation, query } from './_generated/server'
 import { auth } from './auth'
 
+const generateCode = () => {
+  const code = Array.from(
+    { length: 6 },
+    () => '0123456789abcdefghijklmopqrstuvwxyz'[Math.floor(Math.random() * 36)],
+  ).join('')
+
+  return code
+}
+
 export const create = mutation({
   args: {
     name: v.string(),
@@ -12,13 +21,18 @@ export const create = mutation({
 
     if (!userId) throw new Error('Unauthorized')
 
-    // TODO: Dynamically generate join code
-    const joinCode = '123456'
+    const joinCode = generateCode()
 
     const workspaceId = await ctx.db.insert('workspaces', {
       name: args.name,
       userId,
       joinCode,
+    })
+
+    await ctx.db.insert('members', {
+      role: 'admin',
+      userId,
+      workspaceId,
     })
 
     return { workspaceId }
@@ -28,7 +42,30 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query('workspaces').collect()
+    const userId = await auth.getUserId(ctx)
+
+    if (!userId) {
+      return []
+    }
+
+    const members = await ctx.db
+      .query('members')
+      .withIndex('by_user_id', (q) => q.eq('userId', userId))
+      .collect()
+
+    const workspaceIds = members.map((member) => member.workspaceId)
+
+    const workspaces = []
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId)
+
+      if (workspace) {
+        workspaces.push(workspace)
+      }
+    }
+
+    return workspaces
   },
 })
 
@@ -38,6 +75,14 @@ export const getById = query({
     const userId = await auth.getUserId(ctx)
 
     if (!userId) throw new Error('Unauthorized')
+
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', args.id).eq('userId', userId),
+      )
+
+    if (!member) return null
 
     return await ctx.db.get(args.id)
   },
