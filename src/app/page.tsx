a8b19e2b13ee557caea3cc0b21755111ebcb68f9
gustavo <gustavogarciaci1@gmail.com>
2024-09-01 15:19:37 -0300

    feat: user button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-'use client'
-
-import { useAuthActions } from '@convex-dev/auth/react'
-
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
-  const { signOut } = useAuthActions()
-
-  return (
-    <h1>
-      Logged In! <Button onClick={signOut}>Sign Out</Button>
-    </h1>
-  )
+  return <main>Home</main>
 }
---
+++ src/features/auth/components/user-button.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { useAuthActions } from '@convex-dev/auth/react'
+import { Loader, LogOut } from 'lucide-react'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+import { useCurrentUser } from '../hooks/use-current-user'
+
+export const UserButton = () => {
+  const { data, isLoading } = useCurrentUser()
+  const { signOut } = useAuthActions()
+
+  if (isLoading)
+    return <Loader className="size-4 animate-spin text-muted-foreground" />
+
+  if (!data) return null
+
+  const { name, image } = data
+
+  const avatarFallback = name ? name.charAt(0).toUpperCase() : 'US'
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="relative outline-none">
+        <Avatar className="size-10 transition hover:opacity-75">
+          <AvatarImage src={image} />
+          <AvatarFallback className="bg-indigo-500 text-zinc-50">
+            {avatarFallback}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center" side="right" className="w-60">
+        <DropdownMenuItem onClick={signOut} className="h-10">
+          <LogOut className="mr-2 size-4" />
+          Sair
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
